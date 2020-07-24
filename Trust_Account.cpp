f4f0e5d8aec53fd3d15d9579b01ce8cc46b40f13
william a feliciano rios <31602072+wfeliciano20@users.noreply.github.com>
2020-07-24 19:43:22 -0400

    Add files via upload
    editted

--- Trust_Account.cpp
+++ Trust_Account.cpp
@@ -27,7 +27,7 @@ bool Trust_Account::withdraw(double amount)
     }
 }
 
-std::ostream &operator<<(const std::ostream &out, const Trust_Account &account)
+std::ostream &operator<<(std::ostream &out, const Trust_Account &account)
 {
     out << "[Trust Account: " << account.name << ": " << account.balance << ", " << account.int_rate << "%, withdrawals: " << account.num_withdrawals << "]";
     return out;
